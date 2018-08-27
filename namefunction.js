2ff50b48b8abadd194bda065103794467e9d41b9
Hydra70 <42453477+Hydra70@users.noreply.github.com>
2018-08-27 13:54:44 -1000

    Add files via upload
    HTML Javascript and function demo

---
+++ namefunction.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+</head>
+<body>
+<script>
+function getName() {
+ var userInput = prompt("Name Plz");
+ window.alert(userInput);
+}
+</script>
+<button onclick="getName()">
+Click Me
+</button>
+</body>
